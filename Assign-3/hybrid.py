fa490603ca3267fdff09acdd3a4f3edfc5764074
nithish <nithish@github.com>
2017-08-26 15:52:49 +0530

    Assign-3 Completed

--- Assign-3/hybrid.py
+++ Assign-3/hybrid.py
@@ -3,14 +3,13 @@ from math import ceil;
 import sys;
 class hybrid:
     def __init__(self):
-        self.opt = sys.argv[1];
-        self.pfkey = sys.argv[2];
-        self.rwkey = sys.argv[3];
-        self.text = sys.argv[4];
+        self.opt = sys.argv[1].lower();
+        self.pfkey = sys.argv[2].upper();
+        self.rwkey = sys.argv[3].upper();
+        self.text = sys.argv[4].upper();
         self.check();
 
     def check(self):
-        print(self.opt);
         if self.opt == "encrypt":
             self.encrypt();
         elif self.opt == "decrypt":
@@ -26,6 +25,49 @@ class hybrid:
             self.fill_matrix();
             self.transpose();
 
+    def decrypt(self):
+        keyok = self.check_rtkey();
+        if keyok:
+            self.initializeMat();
+            self.rev_transpose();
+            self.decrypt_mat();
+            playfair().check(self.opt,self.pfkey,self.ct_for_pf);
+
+    def decrypt_mat(self):
+        temp_ct = "";
+        for i in range(self.row):
+            for j in range(self.col):
+                tmp = self.rev_mat[i][j];
+                if tmp != '$':
+                    temp_ct = temp_ct+tmp;
+        self.ct_for_pf = temp_ct;
+        print("\n\n","\t\t*********  PF Cipher Text *********");
+        print("\n\t",temp_ct);
+
+    def rev_transpose(self):
+        rwkey_array = [int(x) for x in str(self.rwkey)];
+        i = 1;k = 0;
+        while i  <= len(rwkey_array):
+            pos = rwkey_array.index(i);
+            for j in range(self.row):
+                if k < len(self.text):
+                    self.rev_mat[j][pos] = self.text[k];
+                    k = k+1;
+            i = i+1;
+        self.display(self.rev_mat);
+
+    def initializeMat(self):
+        ctlen = len(self.text);
+        self.col = len(self.rwkey);
+        self.row = int(ceil(ctlen/self.col));
+        matrix = [];
+        for i in range(self.row):
+            tmp = [];
+            for j in range(self.col):
+                tmp.insert(j,'$');
+            matrix.append(tmp);
+        self.rev_mat = matrix;
+
     def transpose(self):
         rwkey_array = [int(x) for x in str(self.rwkey)];
         fin_cipher_text = "";i = 1;
@@ -34,7 +76,7 @@ class hybrid:
             for j in range(self.row):
                 fin_cipher_text = fin_cipher_text+self.keyMatrix[j][pos];
             i = i+1;
-        self.display();
+        self.display(self.keyMatrix);
         print("\n\n","\t\t********* Final Cipher Text *********");
         print("\n\t",fin_cipher_text);
 
@@ -69,12 +111,12 @@ class hybrid:
             flag = 0;
         return flag;
 
-    def display(self):
+    def display(self,mat):
         print("\n\n","\t\t********* Row Matrix *********\n");
         for i in range(self.row):
             print("\t\t\t  ",end = '');
             for j in range(self.col):
-                print(self.keyMatrix[i][j],end = ' ');
+                print(mat[i][j],end = ' ');
             print();
         return;
 
